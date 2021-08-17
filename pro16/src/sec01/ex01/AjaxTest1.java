2c8a41af674b7f9ad5d0f641e9bc5704046effe4
ChoiSunAh, 최선아 <70833455+CSN-ah22@users.noreply.github.com>
2021-08-17 18:06:16 +0900

    .val() 메서드 사용,Ajax 사용

---
+++ pro16/src/sec01/ex01/AjaxTest1.java
@@ -0,0 +1,35 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ajaxTest1")
+public class AjaxTest1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			doHandel(request,response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandel(request,response);
+	}
+
+	private void doHandel(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		String param=(String)request.getParameter("param");
+		System.out.println("param="+param);
+		PrintWriter writer = response.getWriter();
+		writer.print("안녕하세요! 서버입니다");
+	
+	}
+}
+
