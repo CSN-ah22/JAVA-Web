d2928f609b9adf44af60dfaac36b2f0f8fa312a6
ChoiSunAh, 최선아 <70833455+CSN-ah22@users.noreply.github.com>
2021-08-22 23:26:09 +0900

    08-22 Ajax 이용해 서버와 JSON 데이터 주고받기 첫번째 커밋
    test04, sec03.ex01폴더 완성

---
+++ pro16/src/sec03/ex01/JsonServlet3.java
@@ -0,0 +1,78 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * Servlet implementation class JsonServlet1
+ */
+@WebServlet("/json3")
+public class JsonServlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		dohandle(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		dohandle(request,response);
+	}
+	
+	private void dohandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter writer = response.getWriter();
+		
+			JSONObject totalObject = new JSONObject();	//새
+			JSONArray membersArray = new JSONArray();	//개구리
+			JSONObject memberInfo = new JSONObject(); //벌레
+			
+			memberInfo.put("name","박지성");
+			memberInfo.put("age","25");
+			memberInfo.put("gender","남자");
+			memberInfo.put("nickname","날센돌이");
+			membersArray.add(memberInfo);
+			
+			memberInfo = new JSONObject();
+			memberInfo.put("name","김연아");
+			memberInfo.put("age","21");
+			memberInfo.put("gender","여자");
+			memberInfo.put("nickname","칼치");
+			membersArray.add(memberInfo);
+			
+			totalObject.put("members", membersArray);
+		
+			JSONArray bookArray = new JSONArray();
+			JSONObject bookInfo = new JSONObject();
+			bookInfo.put("title", "초보자를 위한 자바 프로그래밍");
+			bookInfo.put("writer", "이병승");
+			bookInfo.put("price", "30000");
+			bookInfo.put("genre","IT");
+			bookInfo.put("image", "http://localhost:8090/pro16/image/image1.jpg");
+			bookArray.add(bookInfo);
+
+			totalObject.put("books", bookArray);
+			
+			String jsonInfo = totalObject.toJSONString(); //totalObject를 문자열로 변환
+			System.out.print(jsonInfo);
+			writer.print(jsonInfo);
+		
+	}
+
+}
